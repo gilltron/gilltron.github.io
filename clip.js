bb8efe0d391d92f522773f068bc199f8e28f345f
Axiom <ian@axiomdatascience.com>
2020-06-15 14:23:48 -0800

    file to test

---
+++ clip.js
@@ -0,0 +1,10 @@
+
+var metadata_url = "https://researchworkspace.com/api/metadata/objectfull/5edfcc9c24aa9a002fe0971c?uid=1829643&&lock=true"
+ 
+$.getJSON(metadata_url)
+    .then(metadata => {
+        var contacts = metadata.metadata.values.contacts
+        console.log(contacts.points_of_contact[0]['person']['last_name'])
+    })
+
+
