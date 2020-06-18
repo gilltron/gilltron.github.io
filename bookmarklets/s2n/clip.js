4a045c51a1619c0afc4bb00ebd3bcff5036a5658
Axiom <ian@axiomdatascience.com>
2020-06-18 13:25:25 -0800

    setTimeout for the win

--- bookmarklets/s2n/clip.js
+++ bookmarklets/s2n/clip.js
@@ -1,9 +1,6 @@
-
-//var metadata_url = navigator.clipboard.readText()
-//var metadata_url = window.prompt("Enter the URL of your metadata record: ");
-//alert("Your name is " + name);
-//var metadata_url = "https://researchworkspace.com/api/metadata/objectfull/5edfcc9c24aa9a002fe0971c?uid=1829643&&lock=true"
 var metadata_url = "https://researchworkspace.com/files/6472827/metadata.json"
+
+
 
 $.getJSON(metadata_url)
     .then(metadata => {
@@ -19,6 +16,7 @@ $.getJSON(metadata_url)
         var project_program = values.contacts.credits[1];
 
         // insert contacts
+        function deez_contacts(){
         var contact_field = document.forms[1]['first_input']
         if(typeof(contact_field) != 'undefined' && contact_field != null){
             for (var i in contacts) {
@@ -28,34 +26,48 @@ $.getJSON(metadata_url)
                 $('input[name="email"]').val(contacts[i].address.email);
                 $('input[name="institution"]').val(contacts[i].position.organization);
 
-                saveInput['person'];
+                //setTimeout(function after2seconds(){
+                        //saveInput['person'];
+                $('.persons > button').click();
+                //}, 2000)
                 //$('.persons > button').click();
 
 
                 //if(i != 0) {
                 //$(document.getElementById("addAnotherperson").click());
                // }
-
-              
             }    
         }
-        else {console.log("poop13")
         }
-
         //insert funding agency
-        var field = document.forms[2]['funding_agency_input']
-        if(typeof(field) != 'undefined' && field != null) {
-            $('input[name="funding_agency_input"]').val(funding_agency);
-            saveInput('funding_agency');
+        function deez_funders(){
+                var field = document.forms[2]['funding_agency_input']
+                if(typeof(field) != 'undefined' && field != null) {
+                        $('input[name="funding_agency_input"]').val(funding_agency);
+                        saveInput('funding_agency');
+                        console.log('funders')
+        }
         }
 
         //insert related projects and/or programs
-        var field = document.forms[3]['project_input']
-        if(typeof(field) != 'undefined' && field != null) {
-            $('input[name="project_input"]').val(project_program);
-            saveInput('project');
+        function deez_projects(){
+                var field = $('#project_input')
+                if(field.length) {
+                        
+                    field.val(project_program);
+                        saveInput('project');
+                        
+                } else {
+                        console.log("you fucked up")
+                  }
+        console.log('projects')
         }
+
+
+deez_contacts();
+setTimeout(deez_funders, 2000);
+setTimeout(deez_projects, 4000);
+
 })
 
 //localStorage.clear()
-
