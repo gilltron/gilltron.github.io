ebb69b72f4b38fc935753dbc848a01dc280343a7
Axiom <ian@axiomdatascience.com>
2020-06-19 14:56:16 -0800

    good through page 2

--- bookmarklets/s2n/s2n.js
+++ bookmarklets/s2n/s2n.js
@@ -14,16 +14,17 @@ $.getJSON(metadata_url)
                 principalInvestigator:'principal investigator',
         }
         var funding_agency = values.contacts.credits[0];
+
         var project_program = values.contacts.credits[1];
 
         //VARS PAGE 2
         var time_range = values.time_period.coverage[0].range;
         var start_date = time_range.start.year + "-" + time_range.start.month + "-" + time_range.start.day;
         var end_date = time_range.end.year + "-" + time_range.end.month + "-" + time_range.end.day;
-        var north_boundary = values.spatial.bounds_and_description.bounding_box.n;
-        var south_boundary = values.spatial.bounds_and_description.bounding_box.s;
-        var east_boundary = values.spatial.bounds_and_description.bounding_box.e;
-        var west_boundary = values.spatial.bounds_and_description.bounding_box.w;
+        var north_boundary = values.spatial.bounds_and_description.bounding_box[0].n;
+        var south_boundary = values.spatial.bounds_and_description.bounding_box[0].s;
+        var east_boundary = values.spatial.bounds_and_description.bounding_box[0].e;
+        var west_boundary = values.spatial.bounds_and_description.bounding_box[0].w;
         var platform = values.contacts.credits[2];
         var sea_name = values.spatial.bounds_and_description.spatial_description;
 
@@ -47,153 +48,136 @@ $.getJSON(metadata_url)
         //FORM PAGE 1
 
         function input_contacts(){
-            //var field = document.forms[1]['first_input']
-            var field = $('first_input')
-                if(field.length) {
-                    for (var i in contacts) {
-                        $('input[name="firstname"]').val(contacts[i].person.first_name);
-                        $('input[name="lastname"]').val(contacts[i].person.last_name);
-                        $('input[name="suffix"]').val(roleMap[contacts[i].role]);
-                        $('input[name="email"]').val(contacts[i].address.email);
-                        $('input[name="institution"]').val(contacts[i].position.organization);
+            for (var i in contacts) {
 
-                        console.log("clicking person button")
-                        $('.persons > button').click();
-                    }    
-                } else {
-                        console.log("no contact field")
-                }
+                $('input[name="firstname"]').val(contacts[i].person.first_name);
+                $('input[name="lastname"]').val(contacts[i].person.last_name);
+                $('input[name="suffix"]').val(roleMap[contacts[i].role]);
+                $('input[name="email"]').val(contacts[i].address.email);
+                $('input[name="institution"]').val(contacts[i].position.organization);
+
+                console.log("clicking person button")
+                $('.persons > button').click();
+            }
         }
 
         function input_funders(){
-                //var field = document.forms[2]['funding_agency_input']
-            var field = $('#funding_agency_input')
-                if(field.length) {
-                        field.val(funding_agency);
-                        console.log("click funder button")
-                        saveInput('funding_agency');
-                } else {
-                        console.log("no funder field")
-                }
-        }
+
+                $('input#funding_agency_input').val(funding_agency);
+
+                console.log("clicking funder button")
+                saveInput('funding_agency');
+            }
 
         function input_projects(){
-                var field = $('#project_input')
-                if(field.length) {
-                    field.val(project_program);
-                        console.log("clicking project button")
-                        saveInput('project');
-                } else {
-                        console.log("no project field")
-                }
+
+                $('input#project_input').val(project_program);
+
+                console.log("clicking project button")
+                saveInput('project');
         }
 
         //FORM PAGE 2
 
         function input_dates(){
-                var field = $('#')
-                if(field.length) {
-                    $('input[name="startdate"]').val(start_date);
-                    $('input[name="enddate"]').val(end_date);
-                    console.log("entering dates")
-                        //saveInput();
-                } else {
-                    console.log("no date fields")    
-                }
+
+                 $('input[name="startdate"]').val(start_date);
+                 $('input[name="enddate"]').val(end_date);
+
+                 console.log("entering dates")
         }
 
         function input_location(){
-                var field = $('#')
-                if(field.length) {
-                    $('input[name="nb"]').val(north_boundary);
-                    $('input[name="sb"]').val(south_boundary);
-                    $('input[name="eb"]').val(east_boundary);
-                    $('input[name="wb"]').val(west_boundary);
-                    console.log("entering location boundaries")
-                        //saveInput();
-                } else {
-                        console.log("no boundary fields")
-                }
+
+                 $('input#n_boundary').val(north_boundary);
+                 $('input[name="sb"]').val(south_boundary);
+                 $('input[name="eb"]').val(east_boundary);
+                 $('input[name="wb"]').val(west_boundary);
+
+                 console.log("entering location boundaries")
         }
 
         function input_platform(){
-                var field = $('#formPlatform')
-                if(field.length) {
-                    $('input[id="firstLink"]').val(platform);
-                        saveInput('platform');
-                        console.log("clicking platform button")
-                } else {
-                        console.log("no platform field")
-                }
+
+                 $('input#platform_input').val(platform);
+
+                 console.log("clicking platform button")
+                 saveInput('platform');
         }
 
         function input_seaname(){
-                var field = $('#formSeaName')
-                if(field.length) {
-                    $('input[id="secondLink"]').val(sea_name);
-                        saveInput('sea_name');
-                        console.log("clicking region button")
-                } else {
-                        console.log("no region field")
-                }
+
+                 $('input#sea_name_input').val(sea_name);
+
+                 console.log("clicking region button")
+                 saveInput('sea_name');
+
         }
 
         //FORM PAGE 3
 
         function input_datatypes(){
-                var field = $('#')
-                if(field.length) {
-                    $('input[id="data_type_edit_1"]').val(parameter);
-                    $('input[id="MorC_1"]').val(measured_or_calculated);
-                    $('input[id="unit_1"]').val(units);
-                    $('input[id="obs_type_1"]').val(observation_category);
-                    $('input[id="si_1"]').val(sampling_instrument);
-                    $('input[id="sna_method_1"]').val(sampling_method);
-                    $('input[id="dqi_1"]').val(data_quality_method);
-                        saveInput();
-                        console.log("clicking data type button")
-                } else {console.log("no data type fields")
 
-                }
+                 $('input[id="data_type_edit_1"]').val(parameter);
+                 $('input[id="MorC_1"]').val(measured_or_calculated);
+                 $('input[id="unit_1"]').val(units);
+                 $('input[id="obs_type_1"]').val(observation_category);
+                 $('input[id="si_1"]').val(sampling_instrument);
+                 $('input[id="sna_method_1"]').val(sampling_method);
+                 $('input[id="dqi_1"]').val(data_quality_method);
+
+                 console.log("clicking data type button")
+                 saveInput();
         }
 
         //FORM PAGE 4
 
         function input_packagedescription(){
-                var field = $('#')
-                if(field.length) {
-                    $('input[id="title"]').val(dataset_title);
-                    $('input[id="abstract"]').val(abstract);
-                    $('input[id="author_list"]').val(dataset_author_list);
-                    $('input[id="purpose"]').val(purpose);
-                    $('input[id="reference"]').val(reference);
-                    console.log("entering package description")
-                        //saveInput();
-                } else {
-                        console.log("no package description fields")
-                }
+
+                 $('input[id="title"]').val(dataset_title);
+                 $('input[id="abstract"]').val(abstract);
+                 $('input[id="author_list"]').val(dataset_author_list);
+                 $('input[id="purpose"]').val(purpose);
+                 $('input[id="reference"]').val(reference);
+
+                 console.log("entering package description")
         }
 
         //PAGE 5: upload & submit
                     //$('input[id="file_input_1"').val();//can be link to RW file? prompt user for link?
 
 
-//run page 1
-input_contacts();
-setTimeout(input_funders, 2000);
-setTimeout(input_projects, 4000);
+//check for input then call appropriate function(s) for the page
+if($('#first_input').length) {
 
-//run page 2
-input_dates();
-setTimeout(input_location, 2000);
-setTimeout(input_platform, 4000);
-setTimeout(input_seaname, 6000);
+    console.log("we are on page 1")
+    //run page 1
+    input_contacts();
+    setTimeout(input_funders, 2000);
+    setTimeout(input_projects, 4000);
 
-//run page 3
-input_datatypes();
+    } else if ($('#start_date').length) {
+    
+      console.log("we are on page 2")
+      //run page 2
+      input_dates();
+      input_location();
+      setTimeout(input_platform, 2000);
+      setTimeout(input_seaname, 4000);
 
-//run page 4
-input_packagedescription();
+        } else if ($('#data_type_input').length) {
+      
+          console.log("we are on page 3")
+          //run page 3
+          //input_datatypes();
+
+            } else if ($('textarea#title')) {
+
+            console.log("we are on page 4")
+            //run page 4
+            //input_packagedescription();
+
+}
 
 })
 
