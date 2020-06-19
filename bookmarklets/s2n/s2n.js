0303ac4d5092539f7c54613c13d0335303877737
Axiom <ian@axiomdatascience.com>
2020-06-19 12:33:09 -0800

    all the fields and cut clip.js

--- bookmarklets/s2n/s2n.js
+++ bookmarklets/s2n/s2n.js
@@ -1,100 +1,176 @@
-var metadata_url = window.prompt("Enter the URL of your metadata record: ");
+//var metadata_url = window.prompt("Enter the URL of your metadata record: ");
+var metadata_url = "https://researchworkspace.com/files/6472827/metadata.json"
 
-//var metadata_url = 'https://researchworkspace.com/api/metadata/objectfull/5edfcc9c24aa9a002fe0971c?uid=1829643&&lock=true';
+$.getJSON(metadata_url)
+    .then(metadata => {
 
-//also works: https://researchworkspace.com/api/metadata/objectfull/5edfcc9c24aa9a002fe0971c
+        //VARS PAGE 1
+        var values = metadata.metadata.values;
+        var contacts = values.contacts.citation_contacts;//loop for multiples
+        var roleMap = {
+                author:'principal investigator',
+                originator:'primary point of contact',
+                owner:'principal investigator',
+                principalInvestigator:'principal investigator',
+        }
+        var funding_agency = values.contacts.credits[0];
+        var project_program = values.contacts.credits[1];
 
-$.getJSON(metadata_url).then(metadata=>{
-    var values = metadata.metadata.values,
-        contacts = values.contacts.citation_contacts,
-        citation = contacts[0],
-        roleMap = {
-            principalInvestigator:'principal investigator'
-            //need other roles
-        };
-//        funding_agency = values.dahdahdah;
-//        project_program = values.dahdahdahdoh;
-        time_period = values.time_period;
-        spatial = values.spatial
+        //VARS PAGE 2
+        var time_range = values.time_period.coverage[0].range;
+        var start_date = time_range.start.year + "-" + time_range.start.month + "-" + time_range.start.day;
+        var end_date = time_range.end.year + "-" + time_range.end.month + "-" + time_range.end.day;
+        var north_boundary = values.spatial.bounds_and_description.bounding_box.n;
+        var south_boundary = values.spatial.bounds_and_description.bounding_box.s;
+        var east_boundary = values.spatial.bounds_and_description.bounding_box.e;
+        var west_boundary = values.spatial.bounds_and_description.bounding_box.w;
+        var platform = values.contacts.credits[2];
+        var sea_name = values.spatial.bounds_and_description.spatial_description;
 
-    //PAGE 1: people and projects
-    
-    //responsible persons (make loop for multiples)
-    $('input[name="firstname"]').val(citation.person.first_name);
-    $('input[name="lastname"]').val(citation.person.last_name);
-    $('input[name="email"]').val(citation.address.email);
-    $('input[name="suffix"]').val(roleMap[citation.role]);
-    $('input[name="institution"]').val(citation.position.organization);
-    
-    //save person
-    $('.persons > button').click();
-    
-    //related funding agencies
-    $('input[name="funding_agency_input"]').val(funding_agency);//need to define (keyword?) place
-    
-    //related projects and/or programs
-    $('input[name="project_input"]').val(project_program);//need to define (keyword?) project (AMBON)
-    
-    //PAGE 2: dates and locations
-    
-    //dates
-    $('input[name="startdate"]').val(time_period.coverage[0].range.start);
-    $('input[name="enddate"]').val(time_period.coverage[0].range.end);
+        //VARS PAGE 3
+        var parameter = values.contacts.credits[5];
+        var measured_or_calculated = values.contacts.credits[6];
+        var units = values.contacts.credits[7];
+        var observation_category = values.contacts.credits[8];
+        var sampling_instrument = values.contacts.credits[9];
+        var sampling_method = values.contacts.credits[10];
+        var data_quality_method = values.contacts.credits[11];
 
-    //location (northern boundary, etc)
-    $('input[name="nb"]').val(spatial.bounds_and_description.bounding_box[0].n);
-    $('input[name="sb"]').val(spatial.bounds_and_description.bounding_box[0].s);
-    $('input[name="wb"]').val(spatial.bounds_and_description.bounding_box[0].w);
-    $('input[name="eb"]').val(spatial.bounds_and_description.bounding_box[0].e);
-    
-    //ship names or platform
-    $('input[name="firstLink"]').val();//need to define (keyword?) platform
-    
-    //sea areas or regions
-    $('input[name="secondLink"]').val(spatial.bounds_and_description.spatial_description);
+        //VARS PAGE 4
+        var dataset_title = values.description.title;
+        var abstract = values.description.abstract;
+        var dataset_author_list = contacts[0].person.last_name + ", " + contacts[0].person.first_name;//loop for multiples
+        var purpose = values.description.purpose;
+        var reference = dataset_author_list + ". " + dataset_title + ". " + "Dataset. Research Workspace.";
 
-    //PAGE 3: data types
-    
-    //parameter or variable
-    $('input[id="data_type_edit_1"]').val();//need to define (entity & attribute? or keywords?) discipline
-    
-    // measured or calculated
-    $('input[id="MorC_1"]').val();//measured as default? (also a keyword?) discipline
-    
-    //units
-    $('input[id="unit_1"]').val();//need to define? (entity & attribute? or keyword?) discipline
-    
-    // observation type
-    $('input[id="obs_type_1"]').val();//in situ as default? (keyword?) discipline
-    
-    //sampling instrument
-    $('input[id="si_1"]').val();//need to define (keyword) instrument
-    
-    //sampling and analyzing method
-    $('input[id="sna_method_1"]').val();//need to define (keyword) instrument
-    
-    //data quality method
-    $('input[id="dqi_1"]').val();//need to define (data quality reports? or keyword?) process
-    
-    //PAGE 4: package description
-    
-    //title
-    $('input[id="title"]').val(values.description.title);
-    
-    //abstract
-    $('input[id="abstract"]').val(values.description.abstract);
-    
-    //dataset author list
-    $('input[id="author_list"]').val();//build from contacts
-    
-    //purpose
-    $('input[id="purpose"]').val(values.description.purpose);
-    
-    //reference
-    $('input[id="reference"]').val();//need to define (build from: author list, title, program?)
-    
-    //PAGE 5: upload & submit
-    $('input[id="file_input_1"').val();//can be link to RW file? prompt user for link?
 
- })
+        //FORM PAGE 1
+
+        function input_contacts(){
+            //var field = document.forms[1]['first_input']
+            var field = $('first_input')
+                if(field.length) {
+                    for (var i in contacts) {
+                        $('input[name="firstname"]').val(contacts[i].person.first_name);
+                        $('input[name="lastname"]').val(contacts[i].person.last_name);
+                        $('input[name="suffix"]').val(roleMap[contacts[i].role]);
+                        $('input[name="email"]').val(contacts[i].address.email);
+                        $('input[name="institution"]').val(contacts[i].position.organization);
+
+                        $('.persons > button').click();
+                    }    
+                }
+        }
+
+        function input_funders(){
+                //var field = document.forms[2]['funding_agency_input']
+            var field = $('#funding_agency_input')
+                if(field.length) {
+                        field.val(funding_agency);
+                        saveInput('funding_agency');
+                }
+        }
+
+        function input_projects(){
+                var field = $('#project_input')
+                if(field.length) {
+                    field.val(project_program);
+                        saveInput('project');
+                }
+        }
+
+        //FORM PAGE 2
+
+        function input_dates(){
+                var field = $('#')
+                if(field.length) {
+                    $('input[name="startdate"]').val(start_date);
+                    $('input[name="enddate"]').val(end_date);
+                        //saveInput();
+                }
+        }
+
+        function input_location(){
+                var field = $('#')
+                if(field.length) {
+                    $('input[name="nb"]').val(north_boundary);
+                    $('input[name="sb"]').val(south_boundary);
+                    $('input[name="eb"]').val(east_boundary);
+                    $('input[name="wb"]').val(west_boundary);
+                        //saveInput();
+                }
+        }
+
+        function input_platform(){
+                var field = $('#')
+                if(field.length) {
+                    $('input[id="firstLink"]').val(platform);
+                        saveInput();
+                }
+        }
+
+        function input_seaname(){
+                var field = $('#')
+                if(field.length) {
+                    $('input[id="secondLink"]'.val(sea_name);
+                        saveInput();
+                }
+        }
+
+        //FORM PAGE 3
+
+        function input_datatypes(){
+                var field = $('#')
+                if(field.length) {
+                    $('input[id="data_type_edit_1"]').val(parameter);
+                    $('input[id="MorC_1"]').val(measured_or_calculated);
+                    $('input[id="unit_1"]').val(units);
+                    $('input[id="obs_type_1"]').val(observation_category);
+                    $('input[id="si_1"]').val(sampling_instrument);
+                    $('input[id="sna_method_1"]').val(sampling_method);
+                    $('input[id="dqi_1"]').val(data_quality_method);
+                        saveInput();
+                }
+        }
+
+        //FORM PAGE 4
+
+        function input_packagedescription(){
+                var field = $('#')
+                if(field.length) {
+                    $('input[id="title"]').val(dataset_title);
+                    $('input[id="abstract"]').val(abstract);
+                    $('input[id="author_list"]').val(dataset_author_list);
+                    $('input[id="purpose"]').val(purpose);
+                    $('input[id="reference"]').val(reference);
+                        saveInput();
+                }
+        }
+
+        //PAGE 5: upload & submit
+                    //$('input[id="file_input_1"').val();//can be link to RW file? prompt user for link?
+
+
+//run page 1
+input_contacts();
+setTimeout(input_funders, 2000);
+setTimeout(input_projects, 4000);
+
+//run page 2
+
+//input_dates();
+//setTimeout(input_location, 2000);
+//setTimeout(input_platform, 4000);
+//setTimeout(input_seaname, 6000);
+
+//run page 3
+
+//input_datatypes();
+
+//run page 4
+
+//input_packagedescription():
+
+})
+
 
